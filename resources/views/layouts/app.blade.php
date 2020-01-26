ebe4cba1dc2942524f39527db073780266ba8ade
Mihó Dániel <dev.mihodaniel@gmail.com>
2020-01-26 13:18:30 +0100

    Fixes leftover bell button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,13 +70,6 @@
                     <!-- Authentication Links -->
                     @auth
                         <li class="nav-item dropdown">
-                            <button class="btn btn-sm btn-muted" type="button" id="notificationsDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="icon">
-                                    <i class="fas fa-bell"></i>
-                                </span>
-                            </button>
-                        </li>
-                        <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle text-normal" href="#" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }} <span class="caret"></span>
